bd5cf8e19107fa6e0a9edfc1b934c0af92186915
chla aymen <aymen.chla@gmail.com>
2017-01-28 17:59:47 +0000

    score_board

--- score.h
+++ score.h
@@ -15,10 +15,23 @@
 
     typedef struct{
         int nb_points;
+        int nb_mots;
         //tete de la liste chainée
         Mot *teteMots;
     } Score;
 
+    //pour l'affichage du score
+    typedef struct{
+        char nom_joueur[20];
+        char vs[20];
+        Score score;
+        int nb_secondes;
+    }Score_board;
+
+    typedef struct{
+        Score_board *records;
+        int nb_records;
+    }Tab_Score_board;
 
     int nb_points(char *);
     Mot *creer_mot(char*);
@@ -27,7 +40,15 @@
     boolean rechercher_mot(Score *score,char *mot);
     void afficher_score(Score* );
     void liberation_score(Score *score);
-
+    void sauvegarder_score(char *nom_joueur,char *vs,Score* score,int nb_secondes);
+    void sauvegarder_score_trier(char *nom_joueur,char *vs,Score* score,int nb_secondes);
+    void affichage_score_board();
+    Tab_Score_board best_score_by_time(int start,int nb_records,int nb_secondes);
+    Tab_Score_board best_score_by_player_by_time(int start,int nb_records,int nb_secondes);
+    void recuperer();
+    void afficher_score_board(Tab_Score_board tab_scores);
+    Tab_Score_board get_all_scores();
+    void save_record(Score_board record,FILE *fichier);
 
 
 #endif // SCORE_H
